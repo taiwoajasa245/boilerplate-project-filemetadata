7603b433780212e206d9b1276c3fbcef61b85d21
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-04-22 13:55:47 +0100

    project solution

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config(); 
+
+// multer
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static('public')); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); // to help get the post request from the index.js
+
+
+// routes 
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+const fileUpload = upload.single('upfile'); 
+
+app.post('/api/fileanalyse', fileUpload,  (req, res, next) => {
+  const upfile  = req.file; 
+
+  res.status(200).json({ 
+    name: upfile.originalname, 
+    type: upfile.mimetype, 
+    size: upfile.size
+  }); 
+
+
+  next(); 
+}); 
 
 
 
